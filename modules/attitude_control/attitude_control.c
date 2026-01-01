336aa2d804e6f0d052a468aace6e3ec7aaba0e03
Bao HQ <baofair001@gmail.com>
2026-01-01 23:00:50 +0800

    250hz freq

--- modules/attitude_control/attitude_control.c
+++ modules/attitude_control/attitude_control.c
@@ -17,7 +17,7 @@
 #define MAX_SPEED 3000
 #endif // MOTOR_TYPE
 
-#define PID_FREQ 500
+#define PID_FREQ 250
 
 typedef enum {
 	DISARMED = 0,
@@ -75,15 +75,15 @@ static void move_in_control_update(uint8_t *data, size_t size) {
 
 static void pid_setup(void) {
 	pid_control_init(&g_pid_att_roll);
-	pid_control_set_p_gain(&g_pid_att_roll, 10);
-	pid_control_set_d_gain(&g_pid_att_roll, 5);
+	pid_control_set_p_gain(&g_pid_att_roll, 6);
+	pid_control_set_d_gain(&g_pid_att_roll, 3);
 	pid_control_set_i_gain(&g_pid_att_roll, 1.0, 1.0);
 	pid_control_set_i_limit(&g_pid_att_roll, 5);
 	pid_control_set_smooth(&g_pid_att_roll, 1.0, 1.0, 1.0);
 
 	pid_control_init(&g_pid_att_pitch);
-	pid_control_set_p_gain(&g_pid_att_pitch, 10);
-	pid_control_set_d_gain(&g_pid_att_pitch, 5);
+	pid_control_set_p_gain(&g_pid_att_pitch, 6);
+	pid_control_set_d_gain(&g_pid_att_pitch, 3);
 	pid_control_set_i_gain(&g_pid_att_pitch, 1.0, 1.0);
 	pid_control_set_i_limit(&g_pid_att_pitch, 5);
 	pid_control_set_smooth(&g_pid_att_pitch, 1.0, 1.0, 1.0);
@@ -183,7 +183,7 @@ void attitude_control_setup(void) {
 	publish(SPEED_CONTROL_SETUP, NULL, 0);
 
 	subscribe(SENSOR_ATTITUDE_ANGLE, angular_state_update);
-	subscribe(SCHEDULER_500HZ, attitude_control_loop);
+	subscribe(SCHEDULER_250HZ, attitude_control_loop);
 	subscribe(STATE_DETECTION_UPDATE, state_update);
 	subscribe(COMMAND_SET_MOVE_IN, move_in_control_update); // For motor testing
 	subscribe(COMMAND_SET_TARGET_ORIENTATION, angular_target_update);
